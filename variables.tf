e792e5866b9c45e605dec48b9aec1c0ca788bfc7
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-10-03 11:37:47 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "tag_name" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "identity" {}
+variable "username" {}
 variable "vpc_security_group_ids" {
   type = "list"
 }
