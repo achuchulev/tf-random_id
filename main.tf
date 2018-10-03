728d26a97f4023852ca18c3caf3c9c87a1549263
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-10-03 14:49:52 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "random_id" "server" {
     # Generate a new id each time we switch to a new AMI id
     ami_id = "${var.ami_id}"
   }
+
   byte_length = 8
 }
 
@@ -15,6 +16,7 @@ resource "aws_instance" "server" {
   tags = {
     Name = "web-server ${random_id.server.hex}"
   }
+
   # Read the AMI id "through" the random_id resource to ensure that both will change together.
   ami = "${random_id.server.keepers.ami_id}"
 
