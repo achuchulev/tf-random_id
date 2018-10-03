00e1c508e35eb05824a465f6f787dac7a9e78582
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-10-03 13:35:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "random_id" "server" {
     # Generate a new id each time we switch to a new AMI id
     ami_id = "${var.ami_id}"
   }
-
   byte_length = 8
 }
 
@@ -16,18 +15,11 @@ resource "aws_instance" "server" {
   tags = {
     Name = "web-server ${random_id.server.hex}"
   }
-
   # Read the AMI id "through" the random_id resource to ensure that both will change together.
   ami = "${random_id.server.keepers.ami_id}"
 
-  instance_type = "${var.instance_type}"
-
+  instance_type          = "${var.instance_type}"
   subnet_id              = "${var.subnet_id}"
   vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
   key_name               = "${aws_key_pair.my_key.id}"
-
-  connection {
-    user        = "${var.username}"
-    private_key = "${file("~/.ssh/id_rsa")}"
-  }
 }
