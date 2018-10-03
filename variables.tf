e0102cbbfd6e82858a1b6555af84c581e2e7bb2d
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-10-03 11:06:50 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+# Define variables
+variable "ami" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "region" {}
+variable "tag_name" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "identity" {}
+variable "vpc_security_group_ids" {
+  type = "list"
+}
