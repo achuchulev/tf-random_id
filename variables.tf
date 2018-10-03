09c150172fbec085e6ed17b1b90586934f27969e
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-10-03 14:50:27 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 # Define variables
 variable "ami_id" {}
+
 variable "instance_type" {}
 variable "subnet_id" {}
 variable "region" {}
@@ -7,6 +8,7 @@ variable "tag_name" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "identity" {}
+
 variable "vpc_security_group_ids" {
   type = "list"
 }
